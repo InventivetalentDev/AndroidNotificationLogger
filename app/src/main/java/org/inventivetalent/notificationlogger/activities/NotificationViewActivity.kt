600abc56499c37c38135a97484d6e81da9c8c283
inventivetalent <mail@inventivetalent.org>
2020-02-05 21:35:13 +0100

    add translations of int codes to strings

--- app/src/main/java/org/inventivetalent/notificationlogger/activities/NotificationViewActivity.kt
+++ app/src/main/java/org/inventivetalent/notificationlogger/activities/NotificationViewActivity.kt
@@ -1,5 +1,6 @@
 package org.inventivetalent.notificationlogger.activities
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
@@ -27,9 +28,9 @@ class NotificationViewActivity : AppCompatActivity() {
         setContentView(R.layout.activity_notification_view)
         actionBar?.setDisplayHomeAsUpEnabled(true)
 
-        appIconView =findViewById(R.id.appIconImageView)
+        appIconView = findViewById(R.id.appIconImageView)
         actionIconView = findViewById(R.id.actionIconImageView)
-        notificationTitleView=findViewById(R.id.notificationTitleTextView)
+        notificationTitleView = findViewById(R.id.notificationTitleTextView)
         notificationContentView = findViewById(R.id.notificationContentTextView)
         notificationDateView = findViewById(R.id.notificationDateTextView)
         notificationTimeView = findViewById(R.id.notificationTimeTextView)
@@ -38,7 +39,8 @@ class NotificationViewActivity : AppCompatActivity() {
         val notificationId = intent?.getIntExtra("notificationId", -1)
         if (notificationId != null && notificationId != -1) {
             val viewmodelProvider =
-                ViewModelProvider(this,
+                ViewModelProvider(
+                    this,
                     NotificationViewModelFactory(
                         application
                     )
@@ -48,9 +50,9 @@ class NotificationViewActivity : AppCompatActivity() {
 
             viewmodelProvider.getById(notificationId) { notification ->
                 println(notification)
-                if (notification != null){
+                if (notification != null) {
                     onNotificationLoaded(notification)
-                }else{
+                } else {
                     finish()
                 }
             }
@@ -61,9 +63,10 @@ class NotificationViewActivity : AppCompatActivity() {
     }
 
 
+    @SuppressLint("SetTextI18n")
     @UiThread
     fun onNotificationLoaded(notification: Notification) {
-        val applicationInfo = packageManager.getApplicationInfo(notification.packageName,0)
+        val applicationInfo = packageManager.getApplicationInfo(notification.packageName, 0)
 
         // Same stuff as the list view holder
         appIconView.setImageDrawable(packageManager.getApplicationIcon(applicationInfo))
@@ -82,18 +85,66 @@ class NotificationViewActivity : AppCompatActivity() {
         tagTextView.text = notification.tag
         packageTextView.text = notification.packageName
         tickerTextView.text = notification.tickerText
-        whenTextView.text = if(notification.whenTime!=null) NotificationListAdapter.dateTimeFormat.format(notification.whenTime!!) else "?"
+        whenTextView.text =
+            if (notification.whenTime != null) NotificationListAdapter.dateTimeFormat.format(
+                notification.whenTime!!
+            ) else "?"
         numberTextView.text = notification.number.toString()
-        visibilityTextView.text = notification.visibility.toString()
-        priorityTextView.text = notification.priority.toString()
-        interruptionFilterTextView.text = notification.interruptionFilter.toString()
-        reasonTextView.text = notification.removeReason.toString()
+
+        visibilityTextView.text = when (notification.visibility) {
+            1 -> "PUBLIC"
+            0 -> "PRIVATE"
+            -1 -> "SECRET"
+            else -> "UNKNOWN"
+        } + " (" + notification.visibility.toString() + ")"
+
+        priorityTextView.text = when (notification.priority) {
+            0 -> "NONE"
+            1 -> "MIN"
+            2 -> "LOW"
+            3 -> "DEFAULT"
+            4 -> "HIGH"
+            5 -> "MAX"
+            else -> "UNKNOWN"
+        } + " (" + notification.priority.toString() + ")"
+
+        interruptionFilterTextView.text = when (notification.interruptionFilter) {
+            1 -> "ALL"
+            2 -> "PRIORITY"
+            3 -> "NONE"
+            4 -> "ALARMS"
+            else -> "UNKNOWN"
+        } + " (" + notification.interruptionFilter.toString() + ")"
+
+        reasonTextView.text = when (notification.removeReason) {
+            1 -> "CLICK"
+            2 -> "CANCEL"
+            3 -> "CANCEL_ALL"
+            4 -> "ERROR"
+            5 -> "PACKAGE_CHANGED"
+            6 -> "USER_STOPPED"
+            7 -> "PACKAGE_BANNED"
+            8 -> "APP_CANCEL"
+            9 -> "APP_CANCEL_ALL"
+            10 -> "LISTENER_CANCEL"
+            11 -> "LISTENER_CANCEL_ALL"
+            12 -> "GROUP_SUMMARY_CANCELED"
+            13 -> "GROUP_OPTIMIZATION"
+            14 -> "PACKAGE_SUSPENDED"
+            15 -> "PROFILE_TURNED_OFF"
+            16 -> "UNAUTOBUNDLED"
+            17 -> "CHANNEL_BANNED"
+            18 -> "SNOOZED"
+            19 -> "TIMEOUT"
+            else -> "UNKNOWN"
+        } + " (" + notification.removeReason.toString() + ")"
+
         colorTextView.text = notification.color.toString()
         colorTextView.setTextColor(notification.color)
+
         flagsTextView.text = notification.flags.toString()
 
         extrasJsonTextView.text = notification.extrasJson?.toString(2)
-
 
 
     }
